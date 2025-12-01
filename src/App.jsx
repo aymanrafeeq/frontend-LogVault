a230f87e8bdcc20c07e0ccc19a5831e47d463283
ayman <aymanrafeeque419@gmail.com>
2025-12-01 11:17:05 +0530

    Change the header style

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,10 @@ export default function App() {
   return (
     <div>
       <Provider store={store}>
-        <h1 className="text-center text-6xl font-">LOG VAULT</h1>
+        <h1 className="text-center text-6xl font-extrabold text-cyan-400 drop-shadow-[0_0_20px_rgba(34,211,238,0.6)] tracking-widest py-6">
+          LOG VAULT
+        </h1>
+
         <FilterOptions />
         <LogTable />
       </Provider>
